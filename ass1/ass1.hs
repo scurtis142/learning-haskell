2194fc65eb4e3dd7c5763b7736d12a9a466079d7
Simon Curtis <s.curtis@uq.net.au>
2020-03-24 00:58:45 +1000

    Assignment 1 Progress

--- ass1/ass1.hs
+++ ass1/ass1.hs
@@ -10,7 +10,45 @@
 -- a data type that denotes the numbers that each player may select from
 data Number_1to9 =
    N1 | N2 | N3 | N4 | N5 | N6 | N7 | N8 | N9
+   deriving (Eq,Show)
 
+num129_to_int :: Number_1to9 -> Integer
+num129_to_int N1 = 1
+num129_to_int N2 = 2
+num129_to_int N3 = 3
+num129_to_int N4 = 4
+num129_to_int N5 = 5
+num129_to_int N6 = 6
+num129_to_int N7 = 7
+num129_to_int N8 = 8
+num129_to_int N9 = 9
+
+int_to_num129 :: Integer -> Number_1to9
+int_to_num129 1 = N1
+int_to_num129 2 = N2
+int_to_num129 3 = N3
+int_to_num129 4 = N4
+int_to_num129 5 = N5
+int_to_num129 6 = N6
+int_to_num129 7 = N7
+int_to_num129 8 = N8
+int_to_num129 9 = N9
+int_to_num129 x = error ((show x) ++ " is not a Number_1to9")
+
+
+instance Num Number_1to9 where
+   (+) = \a b -> int_to_num129 (num129_to_int a + num129_to_int b)
+   (-) = \a b -> int_to_num129 (num129_to_int a - num129_to_int b)
+   (*) = \a b -> int_to_num129 (num129_to_int a * num129_to_int b)
+   negate = error "Cant negate Number_1to9"
+   abs n = n
+   signum n = 1
+   fromInteger = int_to_num129
+
+
+--type Number_1to9 = Integer
+
+-- need to write a comment explaining this.
 -- not needed as going to use the inbuild haskell list function
 --data List a =
 --   Nil | Cons a (List a)
@@ -19,11 +57,15 @@ data Number_1to9 =
 data GameState =
    GameState [Number_1to9]
 
+type Player1Moves = [Number_1to9]
+type Player2Moves = [Number_1to9]
+
 data Solution =
    Solution
       Number_1to9
       Number_1to9
       Number_1to9
+   deriving Show
 
 -- the possible results of a valid number selection
 data ValidSelection a =
@@ -40,32 +82,48 @@ data NumberSelected =
 
 
 -- start a new game
---newGame :: GameState
---newGame = []
+newGame :: GameState
+newGame = GameState []
+
+getPlayer1Moves :: GameState -> Player1Moves
+getPlayer1Moves (GameState []) = []
+getPlayer1Moves (GameState (x:[])) = x:[]
+getPlayer1Moves (GameState (x:y:z)) = x:getPlayer1Moves (GameState z)
+
+getPlayer2Moves :: GameState -> Player2Moves
+getPlayer2Moves (GameState []) = []
+getPlayer2Moves (GameState (x:[])) = []
+getPlayer2Moves (GameState (x:y:z)) = y:getPlayer2Moves (GameState z)
 
 -- select a number from the game state
---selectNumber :: Number_1to9 -> GameState -> NumberSelected
---selectNumber number state = if number `elem` state then  AlreadySelected number else number : state
+selectNumber :: Number_1to9 -> GameState -> NumberSelected
+selectNumber n state = if n `elem` state 
+                           then AlreadySelected n
+                           else if 
 
 -- returns whether or not the game state is a draw
 --isDraw :: GameState -> Bool
 
--- Basic implementation, need to review edge cases and the fact that it's super inefficient
-sumNEqual :: [Integer] -> Integer -> Integer -> Bool
-sumNEqual [] 0 0 = True
-sumNEqual [] _ _ = False
-sumNEqual list goal 1 = if goal `elem` list then True else False
-sumNEqual (x:xs) goal count = if sumNEqual xs (goal - x) (count-1) then True else sumNEqual xs goal count
+-- Edge cases
+-- Efficiancy
+-- Variable Names
+-- Elem
+-- Type
+-- Type names
+sumNEqual :: (Num t, Eq t) => [t] -> t -> t -> Maybe [t]
+sumNEqual [] 0 0 = Just []
+sumNEqual [] _ _ = Nothing
+sumNEqual list goal 1 = if goal `elem` list then Just [goal] else Nothing
+sumNEqual (x:xs) goal count = case first of
+   (Just a) -> (Just (x:a))
+   Nothing  -> second
+   where first  = sumNEqual xs (goal - x) (count-1)
+         second = sumNEqual xs goal count
 
 
 --playPick15 :: IO ()
 
 
--- Not actually sure if we need this one. 
-
 -- Determines if the game is finished by any state.
 -- Useful function to implement isDraw.
---isGameOver :: GameState -> Bool
-
-
-
+-- isGameOver :: GameState -> Bool
